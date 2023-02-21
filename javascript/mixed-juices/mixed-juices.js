691dd66ee4d0ef45faa6c426af16705c711fe2a8
dimarique <pochtagreen@gmail.com>
2023-02-21 13:02:59 +0200

    add mixed-juices

--- javascript/mixed-juices/mixed-juices.js
+++ javascript/mixed-juices/mixed-juices.js
@@ -1,76 +1,60 @@
-function timeToMixJuice(name) {
-	let result = 2.5;
-	switch (name) {
-		case 'Pure Strawberry Joy':
-			result = 0.5;
-			break;
-		case 'Energizer':
-		case 'Green Garden':
-			result = 1.5;
-			break;
-		case 'Tropical Island':
-			result = 3;
-			break;
-		case 'All or Nothing':
-			result = 5;
-			break;
-	}
-	return result;
+export function timeToMixJuice(name) {
+    let result = 0
+    switch (name) {
+        case 'Pure Strawberry Joy':
+            result = 0.5
+            break;
+        case 'Energizer':
+        case 'Green Garden':
+            result = 1.5
+            break;
+        case 'Tropical Island':
+            result = 3
+            break;
+        case 'All or Nothing':
+            result = 5
+            break;
+        default:
+            result = 2.5
+            break;
+    }
+    return result
 }
-
-function limesToCut(wedgesNeeded, limes) {
-	let wedgesCounter = wedgesNeeded;
-	let result = 0;
-	limes.forEach((lime) => {
-		if (wedgesCounter > 0) {
-			switch (lime) {
-				case 'small':
-					wedgesCounter -= 6;
-					result++;
-					break;
-				case 'medium':
-					wedgesCounter -= 8;
-					result++;
-					break;
-				case 'large':
-					wedgesCounter -= 10;
-					result++;
-					break;
-			}
-		}
-	});
-	return result;
+export function limesToCut(wedgesNeeded, limes) {
+    if (wedgesNeeded === 0) {
+        return 0
+    }
+    let totalWedges = 0;
+    let numLimes = 0;
+    for (let i = 0; i < limes.length; i++) {
+        let wedges;
+        switch (limes[i]) {
+            case 'small':
+                wedges = 6;
+                break;
+            case 'medium':
+                wedges = 8;
+                break;
+            case 'large':
+                wedges = 10;
+                break;
+        }
+        while (wedges > 0 && totalWedges < wedgesNeeded) {
+            totalWedges++;
+            wedges--;
+        }
+        numLimes++;
+        if (totalWedges >= wedgesNeeded) {
+            break;
+        }
+    }
+    return numLimes;
 }
-
-function remainingOrders(timeLeft, orders) {
-	let result = 0;
-	const drinks = {
-		'Pure Strawberry Joy': 0.5,
-		Energizer: 1.5,
-		'Green Garden': 1.5,
-		'Tropical Island': 3,
-		'All or Nothing': 5,
-	};
-
-	orders.forEach((order) => {
-		if (result <= timeLeft) {
-			switch (order) {
-				case 'Pure Strawberry Joy':
-					result += 0.5;
-					break;
-				case 'Energizer':
-				case 'Green Garden':
-					result += 1.5;
-					break;
-				case 'Tropical Island':
-					result += 3;
-					break;
-				case 'All or Nothing':
-					result += 5;
-					break;
-			}
-		}
-	});
-	return Object.keys(drinks)[2];
+export function remainingOrders(timeLeft, orders) {
+    let ordersLeft = orders
+    while (timeLeft > 0) {
+        timeLeft = timeLeft - timeToMixJuice(ordersLeft[0])
+        ordersLeft.shift()
+    }
+    return ordersLeft;
 }
-// export { timeToMixJuice, limesToCut };
