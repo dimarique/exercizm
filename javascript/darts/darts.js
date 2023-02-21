d16bce2bb31388eb915f2c5daac284d9031981c1
dimarique <pochtagreen@gmail.com>
2023-02-21 19:42:23 +0200

    add darts and triangle

---
+++ javascript/darts/darts.js
@@ -0,0 +1,12 @@
+export const score = (x, y) => {
+    let distance = Math.sqrt(x ** 2 + y ** 2);
+    if (distance > 10) {
+        return 0;
+    } else if (distance > 5) {
+        return 1;
+    } else if (distance > 1) {
+        return 5;
+    } else {
+        return 10;
+    }
+};
